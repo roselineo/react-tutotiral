2cddf91c9f4ae037e903e564f95032b5ea481f0f
roselineo <anyaeler@gmail.com>
2024-03-11 22:22:15 +0100

    initial commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,33 @@
+import { Link } from "react-router-dom"
+import {FaBars , FaTimes} from "react-icons/fa"
 import "./Header.css"
 
 function Header() {
     return(
         <div className="Header">
-            <div className="logo-div"></div>
-            <div className="mobile-menu"></div>
-            <div className="menu"></div>
+            <div className="logo-div">ROSELINE OBINNA</div>
+            <div className="mobile-menu">
+                  <FaBars/>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li>
+                        <Link to="#"className="menu-item">About </Link>
+                    </li>
+                    <li>
+                        <Link to="#"className="menu-item">Skills</Link>
+                    </li>
+                    <li>
+                        <Link to="#"className="menu-item">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="#"className="menu-item">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="#"className="menu-item">Download CV</Link>
+                    </li>
+                </ul>
+            </div>
 
         </div>
     )
